6193b674a1da3894ac211525cd29bc0cafbb5f07
Azat Mardan <hi@azat.co>
2016-08-10 12:34:30 -0700

    add changes :blush:

--- index-advanced.js
+++ index-advanced.js
@@ -10,7 +10,7 @@ const pushOps = {
     accept: '*/*'
   },
   response: {
-    'content-type': 'application/javascript'
+    'Content-Type': 'application/javascript'
   }
 }
 
@@ -20,30 +20,30 @@ fs.readdir('public', (error, data)=>{
     files[`${name}`]=fs
       .readFileSync(path.join(__dirname, 'public', `${name}`), {encoding: 'utf8'})
       .split('\n')
-      .filter(line=>line.match(/script *?src *?= *?"(.*)"/)!=null)
-      .map(line=>line.match(/script *?src *?= *?"(.*)"/)[1])
+      .filter(line=>line.match(/src *?= *?"(.*)"/)!=null)
+      .map(line=>line.match(/src *?= *?"(.*)"/)[1])
   })
-  // console.log(files)
 })
+
 const logger = require('morgan')
 app.use(logger('dev'))
 app.use((request, response, next)=>{
-
   let urlName = url.parse(request.url).pathname.substr(1)
-  // console.log(urlName, files)
+  console.log('Request for: ', urlName)
   if (urlName === '' || urlName === '/') urlName = 'index.html'
   if (files[urlName]) {
-    // let promises = []
-    let promises = files[urlName]
+    // let assets = []
+    let assets = files[urlName]
       .filter(name=>(name.substr(0,4)!='http'))
       .map((fileToPush)=>{
         let fileToPushPath = path.join(__dirname, 'public', fileToPush)
         return (cb)=>{
           fs.readFile(fileToPushPath, (error, data)=>{
             if (error) return cb(error)
-            console.log('Pushing', fileToPush, fileToPushPath)
+            console.log('Will push: ', fileToPush, fileToPushPath)
             try {
-              response.push(`/${fileToPush}`, pushOps).end(data)
+              response.push(`/${fileToPush}`, {}).end(data)
+              // response.push(`/${fileToPush}`, pushOps).end(data) 
               cb()
             } catch(e) {
               cb(e)
@@ -52,18 +52,10 @@ app.use((request, response, next)=>{
           })
         }
       })
-    console.log(promises)
-    // promises = []
-    // promises.push((cb)=>{
-    //   fs.readFile(path.join(__dirname, 'public', urlName), (error, data)=>{
-    //     if (error) return cb(error)
-    //     response.write(data)
-    //     cb()
-    //   })
-    // })
-    // promises.reverse()
-    require('neo-async').parallel(promises, (results)=>{
-      console.log('end', results)
+    console.log('Total number of assets to push: ', assets.length)
+
+    require('neo-async').parallel(assets, (results)=>{
+      // console.log('end', results)
       fs.readFile(path.join(__dirname, 'public', urlName), (error, data)=>{
         if (error) return cb(error)
         response.write(data)
